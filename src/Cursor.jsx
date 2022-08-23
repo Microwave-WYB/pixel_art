ba0abc8d330a2f690feb70709333dcbbbbf4db6f
Yibo Wei <david_wyb2001@outlook.com>
2022-08-23 08:46:25 -0700

    Update: styles

--- src/Cursor.jsx
+++ src/Cursor.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { pixelSize } from "./Board";
 
 const Cursor = (props) => {
   const style = {
     width: "30px",
     height: "30px",
     zIndex: 999,
-    left: props.col * pixelSize,
-    top: props.row * pixelSize,
+    left: props.col * props.pixelSize,
+    top: props.row * props.pixelSize,
     position: "absolute",
-    boxSizing: "border-box",
-    border: props.visible ? "4px dashed rgba(0, 255, 255, 0.8)" : "none",
-    mixBlendMode: "difference",
+    // boxSizing: "border-box",
+    outline: props.visible ? "6px dashed rgba(255, 50, 50, 0.8)" : "none",
+    // backgroundColor: `rgb(${props.color[0]}, ${props.color[1]}, ${props.color[2]})`,
   };
   return (
     <div
