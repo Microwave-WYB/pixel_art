828990040ae60283cb93206c81113e44bc16f65a
Yibo Wei <david_wyb2001@outlook.com>
2022-08-30 20:45:28 -0700

    Add color context

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import React from "react";
 import "./styles/App.css";
 
 import ToolContext from "./ToolContext";
+import ColorContext from "./ColorContext";
 
 export const PIXEL_WIDTH = 30;
 
 function App() {
   const [boardSize, setBoardSize] = React.useState([16, 16]);
   const [tool, setTool] = React.useState("pencil");
+  const [color, setColor] = React.useState([0, 0, 0, 1]);
   return (
     <ToolContext.Provider value={{tool, setTool}}>
       <p>Current tool: {tool}</p>
+      <p>Current color: {`rgba(${color[0]}, ${color[1]}, ${color[2]}, ${color[3]})`}</p>
       <Board
         numRow={boardSize[0]}
         numCol={boardSize[1]}
